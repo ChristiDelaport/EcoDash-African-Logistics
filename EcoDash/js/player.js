e0058f50bf4106a4c286b3c842bbbe7a2063f773
ChristiDelaport <c.delaport2023@gmail.com>
2026-09-23 16:35:46 +0200

    current world pos and movement v

--- EcoDash/js/player.js
+++ EcoDash/js/player.js
@@ -2,3 +2,11 @@
 //reps the delivery drone
 
 class Player {
+    constructor(x, y) {
+    //current world position on the canvas
+    this.x = x;
+    this.y = y;
+
+    //Current movement velocity on the canvas
+    this.velocityX = 0;
+    this.velocityY = 0;
