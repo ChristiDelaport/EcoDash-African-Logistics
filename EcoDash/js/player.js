4f330191e0a45a472dc387079a801b9017d998e0
ChristiDelaport <c.delaport2023@gmail.com>
2026-09-23 16:38:07 +0200

    adding physics to player constructor

--- EcoDash/js/player.js
+++ EcoDash/js/player.js
@@ -10,3 +10,10 @@ class Player {
     //Current movement velocity on the canvas
     this.velocityX = 0;
     this.velocityY = 0;
+
+    this.facingAngle = 0; //radians, 0 = facing right
+    this.thrustPower = 220;
+    //pixels/sec^2 while a direction key is held
+    this.dragFactor = 0.92; 
+    //velocity multiplier applied each frame
+    this.maxSpeed = 220;  // pixels/sec
